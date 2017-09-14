a258ca9b717e69a44b4d780e2246bd0c9aeb4b55
Jordan Mynes <jmynes@otsads.iit.edu>
2017-09-13 19:12:44 -0500

    First commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+Jordan's notes:
+
+https://www.binance.com/restapipub.html
+
+https://github.com/sammchardy/python-binance
+
+pip install python-binance
+
+And simply run jordank.py
+
+For Windows:
+You can use git-bash portable and be mostly done with setup.
+
+Linux:
+Make sure python, git, and a shell are installed.
---
+++ jordank.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+#enter your keys here
+api_key = 'REDACTED'
+api_secret = 'REDACTED'
+
+from binance.client import Client
+client = Client(api_key, api_secret)
+
+time_res = client.get_server_time()
+
+# Working examples
+
+# orders = client.get_all_orders(symbol='BNBBTC', limit=10)
+# print(orders)
+
+# tickers = client.get_all_tickers()
+# print(tickers)
+
+# openorders = client.get_open_orders()
+# print(openorders)
+
+RECV_WINDOW=6000000
+
+class Monitor:
+    def __init__(self):
+        self.bac = Client(api_key,api_secret)
+    def my_balance(self):
+        print(self.bac.get_account(recvWindow=RECV_WINDOW))
+
+
+m = Monitor()
+m.my_balance()
